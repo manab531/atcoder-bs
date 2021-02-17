b18a754f25eac03f2e1888e16e2b0997fa0b6bda
AP19053 <AP19053@azec.ac.jp>
2021-02-17 14:28:38 +0900

    8878b

---
+++ abc0887a.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=int(input())
+c=int(input())
+x=int(input())
+num_combination=0
+for n500 in range(0,a+1):
+    for n100 in range(0,b+1):
+        for n50 in range(0,c+1):
+            amount=n500+500+n100+100+n50+50
+            print(n500,n100,n50,amount)
+            if amount==x:
+                mum_combination+=1
+
+
+print(num_combination)
